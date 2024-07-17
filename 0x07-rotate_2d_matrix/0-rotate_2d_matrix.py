1f364e12328fe26159d03b36d451bb5f6126635a
faith481 <faithayeni@gmail.com>
2024-07-16 18:57:55 -0700

    Rotate 2D matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,10 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
-"""
+"""Module for performing in-place rotation of a 2D matrix"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
-    """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    """In-place rotation of a rectangular 2D list (matrix)"""
+    size = len(matrix[0])
+    for col in range(size - 1, -1, -1):
+        for row in range(size):
+            matrix[row].append(matrix[col].pop(0))
